d06864f2f0cb8f1ea2b9d72071219771a150fcf3
Manish <manishkumaryadav189@gmail.com>
2020-06-11 18:47:15 +0530

    add welcome

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const userController = require("../controller/userController");
 
-const userRoutes = function(app) {
-    const router = express.Router();
-    router.route("/list").get(userController.allUsers);
-    router.route("/saveData").post(userController.saveUser);
-    router.route("/update").put(userController.updateRecord);
-    router.route("/delete").delete(userController.deleteData);
-    
-    return router;
-  };
-  module.exports = userRoutes;
+const userRoutes = function (app) {
+  const router = express.Router();
+  router.route("/list").get(userController.allUsers);
+  router.route("/saveData").post(userController.saveUser);
+  router.route("/update").put(userController.updateRecord);
+  router.route("/delete").delete(userController.deleteData);
+  router.route('/',).get((req, res) => {
+    res.json('WELCOME TO NTY CRUD APPLICATION')
+  })
+
+  return router;
+};
+module.exports = userRoutes;
