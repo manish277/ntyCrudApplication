e710d548a10d89c74d58688e89968e07eae0e411
Manish <manishkumaryadav189@gmail.com>
2020-06-14 22:22:21 +0530

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
-app.use(cors())
+// app.use(cors())
 
 //Connect to Mongo DB
 mongoose
